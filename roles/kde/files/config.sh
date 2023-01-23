81c3895473c7637032df2a5c7ee9149be79a4003
Ben Fiola <benfiola@gmail.com>
2023-01-23 10:15:37 -0800

    Add krunner configuration to kde role

--- roles/kde/files/config.sh
+++ roles/kde/files/config.sh
@@ -10,6 +10,23 @@ kwriteconfig5 --file "$HOME/.config/kdeglobals" --group KDE --key ScrollbarLeftC
 kwriteconfig5 --file "$HOME/.config/kcminputrc" --group Keyboard --key RepeatDelay 200
 kwriteconfig5 --file "$HOME/.config/kcminputrc" --group Keyboard --key RepeatRate 40
 kwriteconfig5 --file "$HOME/.config/kglobalshortcutsrc" --group org.kde.krunner.desktop --key _launch 'Alt+Space\tAlt+F2\tSearch\tCtrl+Space,Alt+Space\tAlt+F2\tSearch,KRunner'
+kwriteconfig5 --file "$HOME/.config/krunnerrc" --group General --key FreeFloating --type bool true
+kwriteconfig5 --file "$HOME/.config/krunnerrc" --group Plugins --key PowerDevilEnabled --type bool false
+kwriteconfig5 --file "$HOME/.config/krunnerrc" --group Plugins --key appstreamEnabled --type bool false
+kwriteconfig5 --file "$HOME/.config/krunnerrc" --group Plugins --key baloosearchEnabled --type bool false
+kwriteconfig5 --file "$HOME/.config/krunnerrc" --group Plugins --key bookmarksEnabled --type bool false
+kwriteconfig5 --file "$HOME/.config/krunnerrc" --group Plugins --key desktopsessionsEnabled --type bool false
+kwriteconfig5 --file "$HOME/.config/krunnerrc" --group Plugins --key helprunnerEnabled --type bool false
+kwriteconfig5 --file "$HOME/.config/krunnerrc" --group Plugins --key krunner_killEnabled --type bool false
+kwriteconfig5 --file "$HOME/.config/krunnerrc" --group Plugins --key kwinEnabled --type bool false
+kwriteconfig5 --file "$HOME/.config/krunnerrc" --group Plugins --key org.kde.activities2Enabled --type bool false
+kwriteconfig5 --file "$HOME/.config/krunnerrc" --group Plugins --key org.kde.windowedwidgetsEnabled --type bool false
+kwriteconfig5 --file "$HOME/.config/krunnerrc" --group Plugins --key placesEnabled --type bool false
+kwriteconfig5 --file "$HOME/.config/krunnerrc" --group Plugins --key plasma-desktopEnabled --type bool false
+kwriteconfig5 --file "$HOME/.config/krunnerrc" --group Plugins --key recentdocumentsEnabled --type bool false
+kwriteconfig5 --file "$HOME/.config/krunnerrc" --group Plugins --key shellEnabled --type bool false
+kwriteconfig5 --file "$HOME/.config/krunnerrc" --group Plugins --key windowsEnabled --type bool false
+
 
 # 'kwriteconfig5' will internally convert '\t' to '\\t' which is incorrect
 # the following command reverts this behavior
