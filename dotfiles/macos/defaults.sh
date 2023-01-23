64d5ae898389ed70ec1bb6ca313fe1d806a92f73
Ben Fiola <benfiola@gmail.com>
2023-01-23 10:15:35 -0800

    Step down keyrepeat settings for macos

--- dotfiles/macos/defaults.sh
+++ dotfiles/macos/defaults.sh
@@ -8,8 +8,8 @@ defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
