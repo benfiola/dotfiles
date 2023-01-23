65ba2f0059b65e95480dae41e1fb359729d8c72a
Ben Fiola <benfiola@gmail.com>
2023-01-23 10:15:36 -0800

    Add shortcut for krunner

--- roles/kde/files/config.sh
+++ roles/kde/files/config.sh
@@ -9,3 +9,8 @@ kwriteconfig5 --file "$HOME/.config/kdeglobals" --group KDE --key SingleClick --
 kwriteconfig5 --file "$HOME/.config/kdeglobals" --group KDE --key ScrollbarLeftClickNavigatesByPage --type bool false
 kwriteconfig5 --file "$HOME/.config/kcminputrc" --group Keyboard --key RepeatDelay 200
 kwriteconfig5 --file "$HOME/.config/kcminputrc" --group Keyboard --key RepeatRate 40
+kwriteconfig5 --file "$HOME/.config/kglobalshortcutsrc" --group org.kde.krunner.desktop --key _launch 'Alt+Space\tAlt+F2\tSearch\tCtrl+Space,Alt+Space\tAlt+F2\tSearch,KRunner'
+
+# 'kwriteconfig5' will internally convert '\t' to '\\t' which is incorrect
+# the following command reverts this behavior
+sed -i 's/\\\\t/\\t/g' ~/.config/kglobalshortcutsrc
