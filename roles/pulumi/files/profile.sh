25852ac281004f8788be7a4f1d478c4c343bb88b
Ben Fiola <benfiola@gmail.com>
2023-04-07 19:27:50 -0700

    Do not auto-generate random pulumi local password (if deleted, will break all pulumi stacks associated with it)

--- roles/pulumi/files/profile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh -e
-secret_file="${ZSHRC_PROFILED_PATH}/pulumi-secret"
-if [ ! -f "${secret_file}" ]; then
-    debug_msg "generating pulumi secret"
-    echo "export PULUMI_CONFIG_PASSPHRASE=$(head -c 24 /dev/urandom | base64)" > "${secret_file}"
-fi
-source "${secret_file}"
