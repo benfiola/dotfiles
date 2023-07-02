1fef258eb8ef07e4a4d4b69723a15e18e3ad706d
Ben Fiola <benfiola@gmail.com>
2023-07-01 17:01:44 -0700

    Add k9s role for archlinux + macos, add gcloud installation for linux

--- roles/gcloud/files/profile.sh
+++ roles/gcloud/files/profile.sh
@@ -1,7 +1,13 @@
 #!/bin/zsh -e
-completions="${HOMEBREW_PREFIX}/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+if sw_vers > /dev/null 2>&1; then
+    completions="${HOMEBREW_PREFIX}/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+else
+    completions="/opt/google-cloud-sdk/path.zsh.inc"
+fi
+
 if [ ! -f "${completions}" ]; then
     debug_msg "no google-cloud-sdk completions found"
     return
 fi 
+
 source "${completions}"
