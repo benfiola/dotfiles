2ed866b0d2378867ab627c7b589aca1627514619
Ben Fiola <benfiola@gmail.com>
2023-07-08 17:37:41 -0700

    Install brew on linux, use brew to install k9s on linux

--- roles/brew/files/profile.sh
+++ roles/brew/files/profile.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env sh
-if ! sw_vers > /dev/null 2>&1; then
-    debug_msg "machine is not macos"
-    return
-fi
 
-brew_path="/usr/local/bin/brew"
-_arch="$(arch)"
-if [ "$_arch" = "arm64" ]; then
-    brew_path="/opt/homebrew/bin/brew"
+if ! sw_vers > /dev/null 2>&1; then
+    # set linux path
+    brew_path="/home/linuxbrew/.linuxbrew/bin/brew"
+else
+    # set darwin path (arm64/amd64)
+    brew_path="/usr/local/bin/brew"
+    _arch="$(arch)"
+    if [ "$_arch" = "arm64" ]; then
+        brew_path="/opt/homebrew/bin/brew"
+    fi
 fi
 
 if [ ! -f "$brew_path" ]; then
