9edb5030b3ead333b09464a10c2aa0914e1061e5
Ben Fiola <benfiola@gmail.com>
2025-08-20 15:18:16 -0700

    remove asdf, arduino, gcloud, golang, gcloud, java, k9s, kubectl, nodejs, pgp, pulumi, python roles

--- roles/java/files/profile.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-if asdf which java > /dev/null 2>&1; then
-    . $ASDF_DIR/plugins/java/set-java-home.zsh
-else
-    debug_msg "no asdf java versions"
-    return
-fi
-
-fpath=(${ASDF_DIR}/completions $fpath)
