7e3ff67210609419ff05709ce5f4eae5274aea7e
Ben Fiola <benfiola@gmail.com>
2025-08-25 19:08:50 -0700

    configure the dock with a long autohide delay (for contexts)

--- roles/macos/files/config.sh
+++ roles/macos/files/config.sh
@@ -21,6 +21,8 @@ defaults write com.apple.finder _FXSortFoldersFirst -bool true
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 defaults write com.apple.finder WarnOnEmptyTrash -bool false
+defaults write com.apple.dock autohide-delay -float 15
+defaults write com.apple.dock autohide-time-modifier -float 1.0
 defaults write com.apple.dock minimize-to-application -bool true
 defaults write com.apple.dock show-process-indicators -bool true
 defaults write com.apple.dock persistent-apps -array
