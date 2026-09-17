66e4c4aa3192b5d2214286ec20e2db521e74fac8
Ben Fiola <me@benfiola.com>
2026-09-17 05:41:38 -0700

    Use correct hw clock setting

--- modules/locale/default.nix
+++ modules/locale/default.nix
@@ -11,7 +11,7 @@ in
     lib.mkIf config.locale.enable {
       i18n.defaultLocale = defaultLocale;
       time.timeZone = timeZone;
-      time.hardwareClockLocalTime = true;
+      time.hardwareClockInLocalTime = true;
     };
 
   darwin =
