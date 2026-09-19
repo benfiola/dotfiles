b41472dd3693f5e85e76c4c1b754a9da9178c082
Ben Fiola <me@benfiola.com>
2026-09-18 17:22:11 -0700

    add 'nix extensions' to firefox

--- modules/firefox/default.nix
+++ modules/firefox/default.nix
@@ -9,35 +9,33 @@
     let
       config = host.config;
 
-      # `package` (path to a package.nix producing $out/extension.xpi) builds to a file:// install_url.
-      builtExtensions = lib.mapAttrs (
-        _id: ext:
-        if ext ? package then
-          let
-            built = pkgs.callPackage ext.package { };
-          in
-          (removeAttrs ext [ "package" ]) // {
-            install_url = "file://${built}/extension.xpi";
-          }
-        else
-          ext
-      ) config.firefox.extensions;
+      localExtensions = lib.pipe config.firefox.extensions [
+        lib.attrValues
+        (lib.filter (ext: ext ? package))
+        (map (ext: pkgs.callPackage ext.package { }))
+      ];
+
+      hostedExtensions = {
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "{26e789e7-acf2-4346-9381-ad473c245e43}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/nord-theme/latest.xpi";
+          installation_mode = "force_installed";
+        };
+      } // lib.filterAttrs (_id: ext: !(ext ? package)) config.firefox.extensions;
+
+      librewolf = pkgs.librewolf.override {
+        nixExtensions = if localExtensions == [ ] then null else localExtensions;
+      };
     in
     lib.mkIf config.firefox.enable {
       programs.firefox = {
         enable = true;
-        package = pkgs.librewolf;
+        package = librewolf;
 
-        policies.ExtensionSettings = {
-          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
-            installation_mode = "force_installed";
-          };
-          "{26e789e7-acf2-4346-9381-ad473c245e43}" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/nord-theme/latest.xpi";
-            installation_mode = "force_installed";
-          };
-        } // builtExtensions;
+        policies.ExtensionSettings = hostedExtensions;
       };
     };
 }
