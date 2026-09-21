5381d595ec0700a4a208be32e3a065dffed74bd3
Ben Fiola <me@benfiola.com>
2026-09-21 10:06:07 -0700

    experiment with disabling all extensions, and only allowing specified extensions

--- modules/firefox/default.nix
+++ modules/firefox/default.nix
@@ -16,6 +16,9 @@
       ];
 
       hostedExtensions = {
+        "*" = {
+          installation_mode = "blocked";
+        };
         "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
           installation_mode = "force_installed";
@@ -23,6 +26,10 @@
         "{26e789e7-acf2-4346-9381-ad473c245e43}" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/nord-theme/latest.xpi";
           installation_mode = "force_installed";
+        };
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/uBlock0@raymondhill.net/latest.xpi";
+          installation_mode = "normal_installed";
         };
       }
       // lib.filterAttrs (_id: ext: !(ext ? package)) config.firefox.extensions;
